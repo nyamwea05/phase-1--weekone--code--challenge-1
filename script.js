ccafc0874b1c36a64e10fe0488ab206f0941739e
sandra <nyamweasandra@gmail.com>
2023-03-20 12:12:16 +0300

    Added my project

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const form = document.querySelector('form');
+const document= document.querySelector('form');
 		const resultDiv = document.querySelector('#result');
 
 		form.addEventListener('submit', event => {
